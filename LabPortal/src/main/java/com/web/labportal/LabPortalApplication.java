05a578f1208b911913f62be303b1f16aff291867
19thMarch <nvtqx1@gmail.com>
2026-04-26 15:36:50 +0700

    firstCommit

---
+++ LabPortal/src/main/java/com/web/labportal/LabPortalApplication.java
@@ -0,0 +1,13 @@
+package com.web.labportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabPortalApplication.class, args);
+    }
+
+}
---
+++ LabPortal/src/test/java/com/web/labportal/LabPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.labportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
