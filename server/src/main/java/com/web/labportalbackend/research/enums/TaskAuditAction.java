e8c81b648796af53e129942d9870450c0eabb066
19thMarch <nvtqx1@gmail.com>
2026-07-08 12:39:55 +0700

    P1-T5: Add TaskAuditAction audit enum

---
+++ server/src/main/java/com/web/labportalbackend/research/enums/TaskAuditAction.java
@@ -0,0 +1,11 @@
+package com.web.labportalbackend.research.enums;
+
+public enum TaskAuditAction {
+    TASK_CREATED,
+    TASK_ASSIGNED,
+    TASK_STATUS_CHANGED,
+    TASK_CANCELLED,
+    TASK_BLOCKED,
+    TASK_COMPLETED,
+    TASK_METADATA_UPDATED
+}
